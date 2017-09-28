1b7a78dfa2243bda6c3f9001be282e1ae31e93fe
Ulf Sjodin <sjodin.ulf@gmail.com>
2017-09-28 23:06:44 +0200

    Update test_game.py
    A pytest for class Room(object):

--- test_game.py
+++ test_game.py
@@ -1,26 +1,6 @@
-class Room(object):
+# Will write a Pytest for: class Room(object):
 
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
-        self.paths = {}
-
-    def go(self, direction):
-        return self.paths.get(direction, None)
-
-    def add_paths(self, paths):
-        self.paths.update(paths)
-
-
-import pytest
-
-def test_room():
-    gold = Room("GoldRoom", 
-                           """This room has gold in it you can grab, 
-                              There is a door to the north,""")
-
-    assert gold.name == "GoldRoom"
-    assert gold.paths == {}
+  
 
 @pytest.fixture()
 def test_add_paths():
