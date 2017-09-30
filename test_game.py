eeebe71b08e0c3015605c3e96dfb5e81431a139c
Ulf Sjodin <sjodin.ulf@gmail.com>
2017-09-30 23:35:16 +0200

    Update test_game.py

--- test_game.py
+++ test_game.py
@@ -1,15 +1,4 @@
-class Room(object):
-
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
-        self.paths = {}
-
-    def go(self, direction):
-        return self.paths.get(direction, None)
-
-    def add_paths(self, paths):
-        self.paths.update(paths)
+from Rooms import Room
 
 def test_room():
     gold = Room("GoldRoom", 
