eb9de576c85cff4b157c3617f6609512ad661cb0
Ulf Sjodin <sjodin.ulf@gmail.com>
2017-09-30 23:30:22 +0200

    Update test_game.py
    class and pytest in same file

--- test_game.py
+++ test_game.py
@@ -1,13 +1,40 @@
-# Will write a Pytest for: class Room(object):
+class Room(object):
 
-  
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.paths = {}
 
-@pytest.fixture()
+    def go(self, direction):
+        return self.paths.get(direction, None)
+
+    def add_paths(self, paths):
+        self.paths.update(paths)
+
+def test_room():
+    gold = Room("GoldRoom", 
+                           """This room has gold in it you can grab, 
+                              There is a door to the north,""")
+
+    assert gold.name == "GoldRoom"
+    assert gold.paths == {}
+
 def test_add_paths():
-    center = Room("Center", 'i mitten')
+    pass
+    center_room = Room("Center", "Test room in the center")
+    north_room = Room("North", "Test room in the north")
+    south_room = Room("South", "Test room in the south")
 
-    center.add_paths(['north'] = north)
+    north_room.add_paths({'south': south_room})
+    center_room.add_paths({'north': north_room})
+    center_room.add_paths({'south': south_room})
+    south_room.add_paths({'north': center_room})
 
 
-def test_stigen(test_add_paths):
-    paths == 'north'
+    riktning = north_room.go('south')
+    assert riktning.name == 'South'
+    riktning = riktning.go('north')
+    assert riktning.name == 'Center'
+    riktning = riktning.go('north')
+    assert riktning.name == 'North'
+    # riktning == direction in English
